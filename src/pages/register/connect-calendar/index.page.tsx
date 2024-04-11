acca37aec31903ea751a74237d779240e39a5119
Flávio Dutra <flaviomdutra@outlook.com>
2024-04-11 08:11:03 -0300

    Add authorization scope for GoogleProvider in nextauth.api.ts and useSession hook in connect-calendar/index.page.tsx

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -3,9 +3,11 @@ import { ArrowRight } from 'phosphor-react'
 // import { api } from "../../../lib/axios"
 import { Container, Header } from '../styles'
 import { ConnectBox, ConnectItem } from './styles'
-import { signIn } from 'next-auth/react'
+import { signIn, useSession } from 'next-auth/react'
 
 export default function Register() {
+  const session = useSession()
+
   // async function handleRegister() {
 
   // }
