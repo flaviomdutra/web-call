75fe4d1ab5c4a1df639329577c81a1f09b67f499
Flávio Dutra <flaviomdutra@outlook.com>
2024-04-11 08:18:29 -0300

    Fix typo in authOptions scope for GoogleProvider in nextauth.api.ts

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -9,10 +9,22 @@ export const authOptions: NextAuthOptions = {
       authorization: {
         params: {
           scope:
-            'https://www/googleapis.com/auth/userinfo.email https://www/googleapis.com/auth/userinfo.profile https://www/googleapis.com/auth/calendar',
+            'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
         },
       },
     }),
   ],
+
+  callbacks: {
+    async signIn({ account }) {
+      if (
+        !account?.scope?.includes('https://www.googleapis.com/auth/calendar')
+      ) {
+        return '/register/connect-calendar?error=permissions'
+      }
+
+      return true
+    },
+  },
 }
 export default NextAuth(authOptions)
