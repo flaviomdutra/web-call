5b12e05e4c38144286d2626b30318e6436e3abea
Flávio Dutra <flaviomdutra@outlook.com>
2024-04-22 00:03:01 -0300

    Update profile

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -23,6 +23,7 @@ import { getWeekDays } from '@/utils/get-week-days'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { convertTimeStringToMinutes } from '@/utils/convert-time-string-to-minutes'
 import { api } from '@/lib/axios'
+import { useRouter } from 'next/router'
 
 const timeIntervalsFormSchema = z.object({
   intervals: z
@@ -87,6 +88,7 @@ export default function TimeIntervals() {
       ],
     },
   })
+  const router = useRouter()
 
   const weekDays = getWeekDays()
 
@@ -103,6 +105,8 @@ export default function TimeIntervals() {
     await api.post('/users/time-intervals', {
       intervals,
     })
+
+    await router.push('/register/update-profile')
   }
 
   return (
