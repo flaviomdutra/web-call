9f4b85ea2b605e070cef1582727210ec7f578c06
Flávio Dutra <flaviomdutra@outlook.com>
2024-04-26 08:23:25 -0300

    Refactor Google OAuth token retrieval and usage in getGoogleOAuthToken function

--- src/lib/google.ts
+++ src/lib/google.ts
@@ -1,6 +1,6 @@
-import { access } from 'fs'
 import { prisma } from './prisma'
 import { google } from 'googleapis'
+import dayjs from 'dayjs'
 
 export async function getGoogleOAuthToken(userId: string) {
   const account = await prisma.account.findFirstOrThrow({
@@ -18,6 +18,46 @@ export async function getGoogleOAuthToken(userId: string) {
   auth.setCredentials({
     access_token: account.access_token,
     refresh_token: account.refresh_token,
-    expiry_date: account.expires_at,
+    expiry_date: account.expires_at ? account.expires_at * 1000 : null,
   })
+
+  if (!account.expires_at) {
+    return auth
+  }
+
+  const isTokenExpired = dayjs(account.expires_at * 1000).isBefore(new Date())
+
+  if (isTokenExpired) {
+    const { credentials } = await auth.refreshAccessToken()
+    const {
+      access_token,
+      expiry_date,
+      id_token,
+      refresh_token,
+      scope,
+      token_type,
+    } = credentials
+
+    await prisma.account.update({
+      where: {
+        id: account.id,
+      },
+      data: {
+        access_token,
+        expires_at: expiry_date ? Math.floor(expiry_date / 1000) : null,
+        id_token,
+        refresh_token,
+        scope,
+        token_type,
+      },
+    })
+
+    auth.setCredentials({
+      access_token,
+      refresh_token,
+      expiry_date,
+    })
+  }
+
+  return auth
 }
